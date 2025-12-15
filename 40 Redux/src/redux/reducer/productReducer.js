de39eef06527fe4221e4e32835d1d936276ecbcb
Manas-Kumar-Lal <manaslal2004@gmail.com>
2025-12-15 05:53:15 +0530

    redux final

--- 40 Redux/src/redux/reducer/productReducer.js
+++ 40 Redux/src/redux/reducer/productReducer.js
@@ -1,14 +1,16 @@
+import { ADD_TO_PRODUCT, INCREMENT } from '../constants/productConstant'
+
 const initialState = {
     products: [],
     count: [],
 }
 
 export const productReducer = (state = initialState, action) => {
-    if (action.type === 'ADD_TO_PRODUCT') {
+    if (action.type === ADD_TO_PRODUCT) {
         return {
             products: [...state.products, action.payload]
         };
-    }if (action.type === 'INCREMENT'){
+    } if (action.type === INCREMENT) {
         return {
             ...state,
             count: state.count + 1
